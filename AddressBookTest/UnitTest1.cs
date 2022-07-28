bd8db4812dba62488ee068dcc89f0abe01406255
ovi2410 <guneshwarifegade@gmail.com>
2022-07-28 08:50:27 +0530

    [ovi]Add:Sorting based on city

--- AddressBookTest/UnitTest1.cs
+++ AddressBookTest/UnitTest1.cs
@@ -69,7 +69,7 @@ namespace AddressBookTest
         public void GivenRetrieveQuery_BasedOnCityorState_returnString()
         {
             string expected = "Raksha Parthiban";
-            string actual = dataTableManger.RetrieveBasedOnCityorState("Lucknow", "UP");
+            string actual = dataTableManger.RetrieveBasedOnCityorState("chennai", "TN");
             Assert.AreEqual(actual, expected);
         }
         //Usecase 5: Retrieve count values from DataTable based on City or State
@@ -81,7 +81,15 @@ namespace AddressBookTest
             string actual = dataTableManger.RetrieveCountBasedOnCityorState();
             Assert.AreEqual(actual, expected);
         }
+        //Usecase 6: Sort based on City
+        [TestMethod]
+        [TestCategory("Sort based on City")]
+        public void GivenSortQuery_BasedOnCityandState_returnString()
+        {
+            string expected = "Raksha Shalini ";
+            string actual = dataTableManger.SortBasedOnNameInDataTable("chennai");
+            Assert.AreEqual(actual, expected);
+        }
 
     }
 }
-
