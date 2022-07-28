0d205be37831c7f47bfacd30f305673ff256d4f2
ovi2410 <guneshwarifegade@gmail.com>
2022-07-28 08:44:43 +0530

    [ovi]Add:Retrieve count based on city or state

--- LINQAddressBook/Program.cs
+++ LINQAddressBook/Program.cs
@@ -46,6 +46,9 @@ namespace LINQAddressBook
             //Retrieve based on city or state
             string var3 = dataTableManger.RetrieveBasedOnCityorState("Bareilly", "UP");
             Console.WriteLine("Success" + varl);
+            //count based on city or state
+            string var4 = dataTableManger.RetrieveCountBasedOnCityorState();
+            Console.WriteLine("Success" + varl);
         }
     }
 }
